130af6f17a9a72fbec4bae73aed35fa51f167e05
Andzhey Asryan <5994736+DemonRax@users.noreply.github.com>
2025-07-28 18:42:20 +0200

    Upgrade to FlyOnUI 2x

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { FC, useEffect } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 import { Menu } from './components/Menu.tsx';
 
 import { Home } from './pages/Home.tsx';
 
-import { IStaticMethods } from 'flyonui/flyonui';
-
-declare global {
-    interface Window {
-        HSStaticMethods: IStaticMethods;
-    }
-}
-
 export const App: FC = () => {
-    const location = useLocation();
-
-    useEffect(() => {
-        const loadFlyonui = async () => {
-            await import('flyonui/flyonui');
-
-            window.HSStaticMethods.autoInit();
-        };
-
-        loadFlyonui().catch(() => {
-        });
-    }, [location.pathname]);
-
     return (
         <div className="min-h-screen bg-base-200/60">
             <Menu />
